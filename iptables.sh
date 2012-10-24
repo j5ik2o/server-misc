d1a316a5a5d3e6ba61d3685dde9aff4f0270868d
Junichi Kato <j5ik2o@gmail.com>
2012-10-24 09:41:09 +0000

    refactor

--- iptables.sh
+++ iptables.sh
@@ -7,12 +7,18 @@
 # インタフェース名定義
 LAN=eth0
 
-HTTP_PORT=80
+HTTP_PORT=80,8081,8082
 IDENT_PORT=113
 
+IPTABLES=/sbin/iptables
 #---------------------------------------#
 # 設定終了                              #
 #---------------------------------------#
+
+# アドレスリスト取得
+. /root/script/iptables_functions
+
+IPLISTGET
 
 # 内部ネットワークのネットマスク取得
 LOCALNET_MASK=`LANG=C ifconfig $LAN|sed -e 's/^.*Mask:\([^ ]*\)$/\1/p' -e d`
@@ -22,59 +28,94 @@ LOCALNET_ADDR=`LANG=C netstat -rn|grep $LAN|grep $LOCALNET_MASK|cut -f1 -d' '`
 LOCALNET=$LOCALNET_ADDR/$LOCALNET_MASK
 
 # 初期化
-iptables -F
-iptables -X
+$IPTABLES -F
+$IPTABLES -X
 
 # デフォルトルール(以降のルールにマッチしなかった場合に適用するルール)設定
-iptables -P INPUT   DROP   # 受信はすべて破棄
-iptables -P OUTPUT  ACCEPT # 送信はすべて許可
-iptables -P FORWARD DROP   # 通過はすべて破棄
+$IPTABLES -P INPUT   DROP   # 受信はすべて破棄
+$IPTABLES -P OUTPUT  ACCEPT # 送信はすべて許可
+$IPTABLES -P FORWARD DROP   # 通過はすべて破棄
 
 # 自ホストからのアクセスをすべて許可
-iptables -A INPUT -i lo -j ACCEPT
+$IPTABLES -A INPUT -i lo -j ACCEPT
 
 # 内部からのアクセスをすべて許可
-iptables -A INPUT -s $LOCALNET -j ACCEPT
+$IPTABLES -A INPUT -s $LOCALNET -j ACCEPT
 
 # ---
+# 独自チェイン定義
+# ---
+
+# 日本
+$IPTABLES -N ACCEPT_COUNTRY
+ACCEPT_COUNTRY_MAKE JP
+
+# 中国・韓国・台湾
+$IPTABLES -N DROP_COUNTRY
+DROP_COUNTRY_MAKE CN
+DROP_COUNTRY_MAKE KR
+DROP_COUNTRY_MAKE TW
+
+# モバイルキャリア
+ACCEPT_SOURCE_MAKE SOFTBANK ./carrier/softbank.lst
+ACCEPT_SOURCE_MAKE AU ./carrier/au.lst
+ACCEPT_SOURCE_MAKE DOCOMO ./carrier/docomo.lst
+
+$IPTABLES -N ACCEPT_MOBILE
+$IPTABLES -A ACCEPT_MOBILE -j ACCEPT_SOFTBANK
+$IPTABLES -A ACCEPT_MOBILE -j ACCEPT_AU
+$IPTABLES -A ACCEPT_MOBILE -j ACCEPT_DOCOMO
+
+# モバイルキャリア
+DROP_SOURCE_MAKE SOFTBANK ./carrier/softbank.lst
+DROP_SOURCE_MAKE AU ./carrier/au.lst
+DROP_SOURCE_MAKE DOCOMO ./carrier/docomo.lst
+
+$IPTABLES -N DROP_MOBILE
+$IPTABLES -A DROP_MOBILE -j DROP_SOFTBANK
+$IPTABLES -A DROP_MOBILE -j DROP_AU
+$IPTABLES -A DROP_MOBILE -j DROP_DOCOMO
+
+# デフォルトで日本以外からは受け付けない
+$IPTABLES -A INPUT -j DROP_COUNTRY
 
 # ---
 # 接続済みパケットは許可
 # 内部から行ったアクセスに対する外部からの返答アクセスを許可
 # ---
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPTABLES -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # ---
 # Stealth Scan
 # ---
-iptables -N STEALTH_SCAN
-iptables -A STEALTH_SCAN -j LOG --log-prefix "[STEALTH_SCAN_ATTACK]: "
-iptables -A STEALTH_SCAN -j DROP
+$IPTABLES -N STEALTH_SCAN
+$IPTABLES -A STEALTH_SCAN -j LOG --log-prefix "[IPTABLES STEALTH_SCAN_ATTACK]: "
+$IPTABLES -A STEALTH_SCAN -j DROP
 
-iptables -A INPUT -p tcp --tcp-flags SYN,ACK SYN,ACK -m state --state NEW -j STEALTH_SCAN
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags SYN,ACK SYN,ACK -m state --state NEW -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags ALL NONE -j STEALTH_SCAN
 
-iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN         -j STEALTH_SCAN
-iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST         -j STEALTH_SCAN
-iptables -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN         -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST         -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j STEALTH_SCAN
 
-iptables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j STEALTH_SCAN
-iptables -A INPUT -p tcp --tcp-flags ACK,FIN FIN     -j STEALTH_SCAN
-iptables -A INPUT -p tcp --tcp-flags ACK,PSH PSH     -j STEALTH_SCAN
-iptables -A INPUT -p tcp --tcp-flags ACK,URG URG     -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags ACK,FIN FIN     -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags ACK,PSH PSH     -j STEALTH_SCAN
+$IPTABLES -A INPUT -p tcp --tcp-flags ACK,URG URG     -j STEALTH_SCAN
 
 # ---
 # フラグメントパケットによるポートスキャン, DOS攻撃対策
 # フラグメント化されたパケットはログを記録して破棄
 # ---
-iptables -A INPUT -f -j LOG --log-prefix '[FRAGMENT_ATTACK] : '
-iptables -A INPUT -f -j DROP
+$IPTABLES -A INPUT -f -j LOG --log-prefix '[IPTABLES FRAGMENT_ATTACK] : '
+$IPTABLES -A INPUT -f -j DROP
 
 # ---
 # Ping of Death Attack
 # ---
-iptables -N PING_OF_DEATH
-iptables -A PING_OF_DEATH -p icmp --icmp-type echo-request \
+$IPTABLES -N PING_OF_DEATH
+$IPTABLES -A PING_OF_DEATH -p icmp --icmp-type echo-request \
          -m hashlimit \
          --hashlimit 1/s \
          --hashlimit-burst 10 \
@@ -83,15 +124,15 @@ iptables -A PING_OF_DEATH -p icmp --icmp-type echo-request \
          --hashlimit-name t_PING_OF_DEATH \
          -j RETURN
 
-iptables -A PING_OF_DEATH -j LOG --log-prefix '[PINGDEATH_ATTACK] : '
-iptables -A PING_OF_DEATH -j DROP
-iptables -A INPUT -p icmp --icmp-type echo-request -j PING_OF_DEATH
+$IPTABLES -A PING_OF_DEATH -j LOG --log-prefix '[IPTABLES PINGDEATH_ATTACK] : '
+$IPTABLES -A PING_OF_DEATH -j DROP
+$IPTABLES -A INPUT -p icmp --icmp-type echo-request -j PING_OF_DEATH
 
 # ---
 # SYN Flood Attack
 # ---
-iptables -N SYN_FLOOD # "SYN_FLOOD" という名前でチェーンを作る
-iptables -A SYN_FLOOD -p tcp --syn \
+$IPTABLES -N SYN_FLOOD # "SYN_FLOOD" という名前でチェーンを作る
+$IPTABLES -A SYN_FLOOD -p tcp --syn \
          -m hashlimit \
          --hashlimit 200/s \
          --hashlimit-burst 3 \
@@ -100,16 +141,16 @@ iptables -A SYN_FLOOD -p tcp --syn \
          --hashlimit-name t_SYN_FLOOD \
          -j RETURN
 
-iptables -A SYN_FLOOD -j LOG --log-prefix "[SYN_FLOOD_ATTACK] : "
-iptables -A SYN_FLOOD -j DROP
-iptables -A INPUT -p tcp --syn -j SYN_FLOOD
+$IPTABLES -A SYN_FLOOD -j LOG --log-prefix "[IPTABLES SYN_FLOOD_ATTACK] : "
+$IPTABLES -A SYN_FLOOD -j DROP
+$IPTABLES -A INPUT -p tcp --syn -j SYN_FLOOD
 
 
 # ---
 # HTTP DoS/DDos Attack
 # ---
-iptables -N HTTP_DOS
-iptables -A HTTP_DOS -p tcp -m multiport --dports $HTTP_PORT \
+$IPTABLES -N HTTP_DOS
+$IPTABLES -A HTTP_DOS -p tcp -m multiport --dports $HTTP_PORT \
          -m hashlimit \
          --hashlimit 1/s \
          --hashlimit-burst 100 \
@@ -118,61 +159,54 @@ iptables -A HTTP_DOS -p tcp -m multiport --dports $HTTP_PORT \
          --hashlimit-name t_HTTP_DOS \
          -j RETURN
 
-iptables -A HTTP_DOS -j LOG --log-prefix "[HTTP_DOS_ATTACK] : "
-iptables -A HTTP_DOS -j DROP
+$IPTABLES -A HTTP_DOS -j LOG --log-prefix "[IPTABLES HTTP_DOS_ATTACK] : "
+$IPTABLES -A HTTP_DOS -j DROP
 
-iptables -A INPUT -p tcp -m multiport --dports $HTTP_PORT -j HTTP_DOS
+$IPTABLES -A INPUT -p tcp -m multiport --dports $HTTP_PORT -j HTTP_DOS
 
 # ---
 # IDENT port probe Attack
 # ---
-iptables -A INPUT -p tcp -m multiport --dports $IDENT_PORT -j REJECT --reject-with tcp-reset
-
+$IPTABLES -A INPUT -p tcp -m multiport --dports $IDENT_PORT -j REJECT --reject-with tcp-reset
 
 # ---
 # SSH Brute Force Attack
 # ---
-iptables -A INPUT -p tcp --syn -m multiport --dports $SSH_PORT -m recent --name ssh_attack --set
-iptables -A INPUT -p tcp --syn -m multiport --dports $SSH_PORT -m recent --name ssh_attack --rcheck --seconds 60 --hitcount 5 -j LOG --log-prefix "[SSH_BRUTE_FORCE] : "
-iptables -A INPUT -p tcp --syn -m multiport --dports $SSH_PORT -m recent --name ssh_attack --rcheck --seconds 60 --hitcount 5 -j REJECT --reject-with tcp-reset
+$IPTABLES -A INPUT -p tcp --syn -m multiport --dports $SSH_PORT -m recent --name ssh_attack --set
+$IPTABLES -A INPUT -p tcp --syn -m multiport --dports $SSH_PORT -m recent --name ssh_attack --rcheck --seconds 60 --hitcount 5 -j LOG --log-prefix "[IPTABLES SSH_BRUTE_FORCE] : "
+$IPTABLES -A INPUT -p tcp --syn -m multiport --dports $SSH_PORT -m recent --name ssh_attack --rcheck --seconds 60 --hitcount 5 -j REJECT --reject-with tcp-reset
 
 # ---
 # 全ホスト(ブロードキャストアドレス、マルチキャストアドレス)宛パケットはログを記録せずに破棄
 # ---
-iptables -A INPUT -d 192.168.1.255   -j LOG --log-prefix "[DROP_BROADCAST] : "
-iptables -A INPUT -d 192.168.1.255   -j DROP
-iptables -A INPUT -d 255.255.255.255 -j LOG --log-prefix "[DROP_BROADCAST] : "
-iptables -A INPUT -d 255.255.255.255 -j DROP
-iptables -A INPUT -d 224.0.0.1       -j LOG --log-prefix "[DROP_BROADCAST] : "
-iptables -A INPUT -d 224.0.0.1       -j DROP
+$IPTABLES -A INPUT -d 192.168.1.255   -j LOG --log-prefix "[IPTABLES DROP_BROADCAST] : "
+$IPTABLES -A INPUT -d 192.168.1.255   -j DROP
+$IPTABLES -A INPUT -d 255.255.255.255 -j LOG --log-prefix "[IPTABLES DROP_BROADCAST] : "
+$IPTABLES -A INPUT -d 255.255.255.255 -j DROP
+$IPTABLES -A INPUT -d 224.0.0.1       -j LOG --log-prefix "[IPTABLES DROP_BROADCAST] : "
+$IPTABLES -A INPUT -d 224.0.0.1       -j DROP
 
 # 外部とのNetBIOS関連のアクセスはログを記録せずに破棄
-# ※不要ログ記録防止
-iptables -A INPUT ! -s $LOCALNET -p tcp -m multiport --dports 135,137,138,139,445 -j DROP
-iptables -A INPUT ! -s $LOCALNET -p udp -m multiport --dports 135,137,138,139,445 -j DROP
-iptables -A OUTPUT ! -d $LOCALNET -p tcp -m multiport --sports 135,137,138,139,445 -j DROP
-iptables -A OUTPUT ! -d $LOCALNET -p udp -m multiport --sports 135,137,138,139,445 -j DROP
+$IPTABLES -A INPUT ! -s $LOCALNET -p tcp -m multiport --dports 135,137,138,139,445 -j DROP
+$IPTABLES -A INPUT ! -s $LOCALNET -p udp -m multiport --dports 135,137,138,139,445 -j DROP
+$IPTABLES -A OUTPUT ! -d $LOCALNET -p tcp -m multiport --sports 135,137,138,139,445 -j DROP
+$IPTABLES -A OUTPUT ! -d $LOCALNET -p udp -m multiport --sports 135,137,138,139,445 -j DROP
 
 # ---
 # 全ホストからの入力許可
 # ---
 # icmp
-iptables -A INPUT -p icmp -j ACCEPT
+$IPTABLES -A INPUT -p icmp -j ACCEPT
 
 # ssh
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT_COUNTRY
+$IPTABLES -A INPUT -p tcp --dport 22 -j ACCEPT_COUNTRY
 
-# elmo api
-iptables -A INPUT -p tcp --dport 8081:8082 -j ACCEPT_COUNTRY
-
-# elmo voip
-iptables -A INPUT -p udp --dport 10001 -j ACCEPT_COUNTRY
-
-
+# elmo 
+$IPTABLES -A INPUT -p tcp --dport 8081:8082 -j ACCEPT_MOBILE
+$IPTABLES -A INPUT -p udp --dport 10001 -j ACCEPT_MOBILE
 
 
 # ブロードキャストアドレス宛pingには応答しない
-# ※Smurf攻撃対策
 sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1 > /dev/null
 sed -i '/net.ipv4.icmp_echo_ignore_broadcasts/d' /etc/sysctl.conf
 echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> /etc/sysctl.conf
@@ -193,35 +227,11 @@ do
     echo "net.ipv4.conf.$dev.accept_source_route=0" >> /etc/sysctl.conf
 done
 
-
-
-
-
-# アドレスリスト取得
-. /root/script/iptables_functions
-IPLISTGET
-
-echo "start country"
-# 日本からのアクセスを許可するユーザ定義チェインACCEPT_COUNTRY作成
-iptables -N ACCEPT_COUNTRY
-ACCEPT_COUNTRY_MAKE JP
-# 以降,日本からのみアクセスを許可したい場合はACCEPTのかわりにACCEPT_COUNTRYを指定する
-
-# 中国・韓国・台湾※からのアクセスをログを記録して破棄
-# ※全国警察施設への攻撃元上位３カ国(日本・アメリカを除く)
-# http://www.cyberpolice.go.jp/detect/observation.htmlより
-iptables -N DROP_COUNTRY
-DROP_COUNTRY_MAKE CN
-DROP_COUNTRY_MAKE KR
-DROP_COUNTRY_MAKE TW
-iptables -A INPUT -j DROP_COUNTRY
-echo "end country"
-
 # 上記のルールにマッチしなかったアクセスはログを記録して破棄
-iptables -A INPUT -m limit --limit 1/s -j LOG --log-prefix '[IPTABLES INPUT] : '
-iptables -A INPUT -j DROP
-iptables -A FORWARD -m limit --limit 1/s -j LOG --log-prefix '[IPTABLES FORWARD] : '
-iptables -A FORWARD -j DROP
+$IPTABLES -A INPUT -j LOG --log-prefix '[IPTABLES INPUT] : '
+$IPTABLES -A INPUT -j DROP
+$IPTABLES -A FORWARD -j LOG --log-prefix '[IPTABLES FORWARD] : '
+$IPTABLES -A FORWARD -j DROP
 
 echo "start iptables-save"
 iptables-save -c > /etc/iptables-save
